2041639b87fe573ddba241b5b457895319fd503d
Joshua Wootonn <joshuawootonn@gmail.com>
2018-01-25 17:46:52 -0600

    Added action/action creators for the champions/items/ and summoners

--- src/actions/static-actions.js
+++ src/actions/static-actions.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import * as types from "./types";
 import * as api from "../api";
 
+
+
 export const getVersion = () => {
   // Checking local storage for the cached static data
-  const versionCache = localStorage.getItem("riot_versions");
+  const versionCache = localStorage.getItem("riot_version");
   if (versionCache) {    
     return dispatch => {
       dispatch({
@@ -27,6 +29,105 @@ export const getVersion = () => {
       error => {
         dispatch({
           type: types.GET_VERSION_FAILURE,
+          payload: error
+        });
+        throw error;
+      }
+    );
+  };
+};
+
+export const getChampion = () => {
+  // Checking local storage for the cached static data
+  const championCache = localStorage.getItem("riot_champion");
+  if (championCache) {    
+    return dispatch => {
+      dispatch({
+        type: types.GET_CHAMPION_LOCAL,
+        payload: JSON.parse(championCache)
+      })      
+    };
+  }
+  // If none is found retreive it
+  const championRequest = api.fetchChampion();
+  return dispatch => {
+    dispatch({ type: types.GET_CHAMPION_REQUEST });
+    return championRequest.then(
+      response => {
+        dispatch({
+          type: types.GET_CHAMPION_SUCCESS,
+          payload: response
+        });
+      },
+      error => {
+        dispatch({
+          type: types.GET_CHAMPION_FAILURE,
+          payload: error
+        });
+        throw error;
+      }
+    );
+  };
+};
+
+export const getItem = () => {
+  // Checking local storage for the cached static data
+  const itemCache = localStorage.getItem("riot_item");
+  if (itemCache) {    
+    return dispatch => {
+      dispatch({
+        type: types.GET_ITEM_LOCAL,
+        payload: JSON.parse(itemCache)
+      })      
+    };
+  }
+  // If none is found retreive it
+  const itemRequest = api.fetchVersion();
+  return dispatch => {
+    dispatch({ type: types.GET_ITEM_REQUEST });
+    return itemRequest.then(
+      response => {
+        dispatch({
+          type: types.GET_ITEM_SUCCESS,
+          payload: response
+        });
+      },
+      error => {
+        dispatch({
+          type: types.GET_ITEM_FAILURE,
+          payload: error
+        });
+        throw error;
+      }
+    );
+  };
+};
+
+export const getSummoner = () => {
+  // Checking local storage for the cached static data
+  const summonerCache = localStorage.getItem("riot_summoner");
+  if (summonerCache) {    
+    return dispatch => {
+      dispatch({
+        type: types.GET_SUMMONER_LOCAL,
+        payload: JSON.parse(summonerCache)
+      })      
+    };
+  }
+  // If none is found retreive it
+  const summonerRequest = api.fetchVersion();
+  return dispatch => {
+    dispatch({ type: types.GET_SUMMONER_REQUEST });
+    return summonerRequest.then(
+      response => {
+        dispatch({
+          type: types.GET_SUMMONER_SUCCESS,
+          payload: response
+        });
+      },
+      error => {
+        dispatch({
+          type: types.GET_SUMMONER_FAILURE,
           payload: error
         });
         throw error;
--- src/actions/types.js
+++ src/actions/types.js
@@ -5,14 +5,17 @@ export const GET_VERSION_REQUEST = "get_version_request";
 export const GET_VERSION_SUCCESS = "get_version_success";
 export const GET_VERSION_FAILURE = "get_version_failure";
 
+export const GET_CHAMPION_LOCAL = "get_chmapion_local";
 export const GET_CHAMPION_REQUEST = "get_champion_request";
 export const GET_CHAMPION_SUCCESS = "get_champion_success";
 export const GET_CHAMPION_FAILURE = "get_champion_failure";
 
+export const GET_ITEM_LOCAL = "get_item_local";
 export const GET_ITEM_REQUEST = "get_item_request";
 export const GET_ITEM_SUCCESS = "get_item_success";
 export const GET_ITEM_FAILURE = "get_item_failure";
 
+export const GET_SUMMONER_LOCAL = "get_summoner_local";
 export const GET_SUMMONER_REQUEST = "get_summoner_request";
 export const GET_SUMMONER_SUCCESS = "get_summoner_success";
 export const GET_SUMMONER_FAILURE = "get_summoner_failure";
