336017a7e378f5e02430e8291ab7fba286624479
Joshua Wootonn <joshuawootonn@gmail.com>
2018-02-12 09:08:57 -0600

    Finished the layout of the league-table component

--- src/components/league/league-table-component.js
+++ src/components/league/league-table-component.js
@@ -1,58 +1,50 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 const LeagueOverviewComponent = props => {
   const league = props.league;
-
-  
 
   const tableContents = league.division.map((ele, i) => {
     return (
       <tr key={i}>
         <th>{i}</th>
         <td>
-          <Link to={`/${ele.playerOrTeamName}`}>
-            {ele.playerOrTeamName}
-          </Link>
+          <Link to={`/${ele.playerOrTeamName}`}>{ele.playerOrTeamName}</Link>
         </td>
         <td>{ele.leaguePoints}</td>
         <td>{ele.wins}</td>
-        <td>{ele.losses}</td>      
-        
+        <td>{ele.losses}</td>
       </tr>
     );
   });
 
   return (
-    <div className="tile box is-12">
-      <div className="columns is-vcentered">
-        <div className="column">
-          <h1>
-            {props.league.leagueName}
-          </h1>
-          <table className="table">
-            <thead>
-              <tr>
-                <th></th>
-                <th>
-                  <abbr title="Summoner">Summoner</abbr>
-                </th>                
-                <th>
-                  <abbr title="LP">LP</abbr>
-                </th>
-                <th>
-                  <abbr title="Wins">Wins</abbr>
-                </th>
-                <th>
-                  <abbr title="Losses">Losses</abbr>
-                </th>                
-              </tr>
-            </thead>
-            <tbody>
-              {tableContents}
-            </tbody>
-          </table>
-        </div>
+    <div className="tile box is-12 parent">
+      <div className="middle width100">
+      
+      <h1 className="title">{props.league.leagueName}</h1>
+     
+      <table className="is-fullwidth table ">
+        <thead>
+          <tr>
+            <th />
+            <th>
+              Summoner
+            </th>
+            <th>
+              LP
+            </th>
+            <th>
+              Wins
+            </th>
+            <th>
+              Losses
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContents}</tbody>
+      </table>
       </div>
+      
     </div>
   );
 };
