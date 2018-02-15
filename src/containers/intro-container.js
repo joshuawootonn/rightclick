9c87a1ad3e233920157ee2e60a663e624835b18d
Joshua Wootonn <joshuawootonn@gmail.com>
2018-02-15 17:05:33 -0600

    Made the intro page responsive

--- src/containers/intro-container.js
+++ src/containers/intro-container.js
@@ -1,34 +1,64 @@
 import React from "react";
 import SearchContainer from "./general/search-container";
 import Typography from "material-ui/Typography";
-import {connect} from 'react-redux'
-import * as actions from '../actions';
-import LoadingContainer from "./general/loading-container";
-import Alert from '../components/general/alert';
-class IntroContainer extends React.Component{
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Alert from "../components/general/alert";
+import Grid from "material-ui/Grid";
+import { withStyles } from "material-ui/styles";
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: '100vh'
+  },
+  fullHeight:{
+    height: "100%"
+  },
+  paper: {
+    height: 140,
+    width: 100
+  },
+  control: {
+    padding: theme.spacing.unit * 2
+  },
+
+});
+class IntroContainer extends React.Component {
   handleClick = () => {
     this.props.toggleAlert();
-  }
-  render(){
-
+  };
+  render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Alert handleClick={this.handleClick} render={this.props.alert} />
-        <LoadingContainer />
-        <div className="hero">
-          <Typography variant="display4" gutterBottom>
-            RightClick.gg
-          </Typography>
-          <SearchContainer index={0} />
-        </div>
-      </div>
+      <Grid container className={classes.root}>
+        <Grid item xs={12} className={classes.fullHeight}>
+          <Grid
+            container
+            className={classes.fullHeight}
+            justify="center"
+            direction="column"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="display3" gutterBottom>
+                RightClick.gg
+              </Typography>
+            </Grid>
+            <Grid item>
+              <SearchContainer index={0} />
+            </Grid>
+          </Grid>
+          <Alert handleClick={this.handleClick} render={this.props.alert} />
+        </Grid>
+      </Grid>
     );
   }
-  
-};
+}
 const mapStateToProps = (state, ownProps) => {
   return {
     alert: state.alert
   };
 };
-export default connect(mapStateToProps,actions)(IntroContainer);
+export default withStyles(styles)(
+  connect(mapStateToProps, actions)(IntroContainer)
+);
